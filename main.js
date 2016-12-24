861ce7194a264097fc79c8eb3835268b5dd1497b
jaswon5791 <jasonwon628@gmail.com>
2016-12-24 12:44:48 -0500

    some changes i guess

--- main.js
+++ main.js
@@ -1,17 +1,17 @@
-var $ = require("jquery")
-var Rx = require("rx")
-var m = require("./dictionary")
+const $ = require("jquery")
+const Rx = require("rx")
+const m = require("./dictionary")
 
 const notepad = $("#notepad")
 const cursor = $("#cursor")
 const view = $("#letter")
 
-var p = false
+let p = false
 
-var kd = Rx.Observable.fromEvent(document,'keydown').filter(()=>!p).filter(e=>e.key==" ").publish()
-var ku = Rx.Observable.fromEvent(document,'keyup').filter(e=>e.key==" ").publish()
+const kd = Rx.Observable.fromEvent(document,'keydown').filter(()=>!p).filter(e=>e.key==" ").publish()
+const ku = Rx.Observable.fromEvent(document,'keyup').filter(e=>e.key==" ").publish()
 
-var back = Rx.Observable.fromEvent(document,'keydown').filter(e=>e.key=="Backspace").publish()
+const back = Rx.Observable.fromEvent(document,'keydown').filter(e=>e.key=="Backspace").publish()
 
 back.subscribe(()=>notepad.text((i, t)=>t.slice(0,-1)))
 
@@ -22,14 +22,14 @@ ku.subscribe(e=>view.removeClass("hold"))
 kd.subscribe(e=>p=true)
 ku.subscribe(e=>p=false)
 
-var morse = ku.withLatestFrom(kd,(u,d)=>(u.timeStamp>200+d.timeStamp)?"-":".")
-var letterDone = ku.flatMap((x)=>Rx.Observable.timer(500).takeUntil(kd))
-var space = letterDone.flatMap((x)=>Rx.Observable.timer(500).takeUntil(kd.merge(back))).map(()=>" ")
+const morse = ku.withLatestFrom(kd,(u,d)=>(u.timeStamp>200+d.timeStamp)?"-":".")
+const letterDone = ku.flatMap((x)=>Rx.Observable.timer(500).takeUntil(kd))
+const space = letterDone.flatMap((x)=>Rx.Observable.timer(500).takeUntil(kd.merge(back))).map(()=>" ")
 
 morse.subscribe(e=>view.text(view.text()+(view.text().length>3?"":e) ))
 letterDone.subscribe(e=>view.text(""))
 
-var letters = morse.buffer(letterDone).map(b=>m.getLetter(b.join(""))).merge(space)
+const letters = morse.buffer(letterDone).map(b=>m.getLetter(b.join(""))).merge(space)
 
 letters.subscribe(e=>notepad.append(e))
 
