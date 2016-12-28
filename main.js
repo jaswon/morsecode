d2c7d484b37c50796dd528c1b69ae84ac5b79ad5
jaswon5791 <jasonwon628@gmail.com>
2016-12-27 22:23:12 -0500

    some changes made

--- main.js
+++ main.js
@@ -1,38 +1,31 @@
 const $ = require("jquery")
-const Rx = require("rx")
 const m = require("./dictionary")
-
-const obs = Rx.Observable
+const obs = require("rx").Observable
 
 const notepad = $("#notepad")
-const cursor = $("#cursor")
 const view = $("#letter")
 
-let p = false
-
-const kd = obs.fromEvent(document,'keydown').filter(()=>!p).filter(e=>e.key==" ").publish()
+const kd = obs.fromEvent(document,'keydown').filter(e=>e.key==" ").publish()
 const ku = obs.fromEvent(document,'keyup').filter(e=>e.key==" ").publish()
+
+const k = kd.merge(ku).scan((a,x,i,s) => x.type=="keyup"?1:(a==1?0:-1),1).filter(v=>v>=0)
 
 const back = obs.fromEvent(document,'keydown').filter(e=>e.key=="Backspace").publish()
 
 back.subscribe(()=>notepad.text((i, t)=>t.slice(0,-1)))
 
-obs.interval(1000).subscribe(()=>cursor.toggle())
+obs.interval(1000).subscribe(()=>$("#cursor").toggle())
 
-kd.subscribe(e=>view.addClass("hold"))
-ku.subscribe(e=>view.removeClass("hold"))
-kd.subscribe(e=>p=true)
-ku.subscribe(e=>p=false)
+k.subscribe(e=>view.toggleClass("hold"))
 
-const morse = ku.withLatestFrom(kd,(u,d)=>(u.timeStamp>200+d.timeStamp)?"-":".")
-const letterDone = ku.flatMap((x)=>obs.timer(500).takeUntil(kd))
-const space = letterDone.flatMap((x)=>obs.timer(500).takeUntil(kd.merge(back))).map(()=>" ")
+const morse = k.timeInterval().filter(e=>e.value==1).map(e=>e.interval>200?"-":".")
+const letterDone = k.debounce(200).filter(e=>e==1)
+const wordDone = letterDone.merge(kd).merge(back).debounce(500).filter(e=>e==1).map(()=>" ")
 
 morse.subscribe(e=>view.text(view.text()+(view.text().length>3?"":e) ))
 letterDone.subscribe(e=>view.text(""))
 
-const letters = morse.buffer(letterDone).map(b=>m.getLetter(b.join(""))).merge(space)
-
+const letters = morse.buffer(letterDone).map(b=>m.getLetter(b.join(""))).merge(wordDone)
 letters.subscribe(e=>notepad.append(e))
 
 kd.connect()
